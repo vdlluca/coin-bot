b5a4f3da3abda17ed7a6e7b9555f1df555c27b5c
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-12-08 11:59:12 +0100

    simple chat bot dialog added

--- src/Controller/BotManController.php
+++ src/Controller/BotManController.php
@@ -11,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class BotManController
 {
     private Botman $botMan;
+    private PriceService $priceService;
 
-    public function __construct(BotMan $botMan)
+    public function __construct(BotMan $botMan, PriceService $priceService)
     {
         $this->botMan = $botMan;
+        $this->priceService = $priceService;
     }
 
     /**
@@ -22,6 +24,21 @@ class BotManController
      */
     public function __invoke(): Response
     {
+        # basic
+        $this->botMan->hears('Hello', function(BotMan $bot) {
+            $bot->reply('Hello back.');
+        });
+
+        $this->botMan->hears('How are you?', function(BotMan $bot) {
+            $bot->reply('I am good.');
+        });
+
+        $this->botMan->hears('My name is {name}', function(BotMan $bot, string $name) {
+            $bot->reply(sprintf('Hello %s.', ucwords($name)));
+        });
+
+
+        # fallback
         $this->botMan->fallback(function (BotMan $bot) {
             $bot->reply('Sorry, I don\'t understand!');
         });
